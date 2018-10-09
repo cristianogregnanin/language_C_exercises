ac0123b9f97c1fe96ad355a3e1d1048e4cf18382
cristianogregnanin <cristiano.gregnanin@gmail.com>
2018-10-09 11:23:38 -0700

    es_2

---
+++ es_2/es_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a,b,p,somma = 0;
+		
+	do{	
+		printf("inserisci a\n");
+		scanf("%d",&a);
+		printf("inserisci b\n");
+		scanf("%d",&b);
+		
+		p = a * b;
+		printf("il prodotto è: %d\n\n",p);
+		
+		somma = somma + p;
+	}while(a!=0 || b!=0);
+	
+	printf("la somma è: %d\n\n", somma);
+	
+}
