0becdfaf95cb5f6d5f7878c7bf0c4082424e3a5a
cristianogregnanin <cristiano.gregnanin@gmail.com>
2018-10-16 11:31:36 -0700

    es_8 es_9

---
+++ es_8/es_8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	int n=0;
+	double somma;
+	
+	while (n>=0){
+		printf("inserisci un numero maggiore o uguale di 0\n");
+		scanf("%d",&n);
+		
+		if (n>=0)
+			somma = somma + sqrt(n);
+		else
+			printf("\nnumero errato\n");
+
+	}	
+	
+	printf("\nla somma è: %f\n\n", somma);
+		
+	return 0;
+}
