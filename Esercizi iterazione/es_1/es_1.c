566eca359c98cb9578e59a66e2399e21b7d177ca
cristianogregnanin <cristiano.gregnanin@gmail.com>
2018-10-16 13:15:35 -0700

    esercizi iterazione

---
+++ Esercizi iterazione/es_1/es_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){	
+	
+	int n,s; //dichiarazione variabili di tipo intero	
+	
+	do{    //ripeti
+		printf("inserisci un numero: \n"); //Scrivi inserisci un numero
+		scanf("%d",&n); //leggi n
+	}while(n<=0); //finche n<=0
+	
+	s = n + 1;
+	printf("il successivo vale: %d\n\n", s); //scrivi s
+		
+	return 0; //la funzione main deve ritornare un valore int
+}
