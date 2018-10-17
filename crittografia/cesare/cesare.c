237117613ef72bba1c4cd3d5c118c45557ae2002
cristianogregnanin <cristiano.gregnanin@gmail.com>
2018-10-17 10:50:22 -0700

    cesare sistemi e reti

---
+++ crittografia/cesare/cesare.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#define DIM 100
+
+void cesare(char s, int salto){
+	char alfabeto[21] = {'a','b',......}
+	....
+	....
+	....
+
+}
+
+
+int main(){
+
+	char s[DIM];
+
+	printf("inserisci una stringa");
+	scanf("%s",s);
+
+	int lunghezza = strlen(s);
+	
+	for(int i=0;i<lunghezza;i++){
+		cesare(s[i],3);
+		
+	}
+
+	
+	return 0;
+}
