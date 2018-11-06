65a59e3b0c0b41c1e5df02a6f7b13a73918a03aa
cristianogregnanin <cristiano.gregnanin@gmail.com>
2018-11-06 11:53:44 -0800

    improved es 4

--- Esercizi puntatori/es_4/es_4.c
+++ Esercizi puntatori/es_4/es_4.c
@@ -1,33 +1,54 @@
 #include <stdio.h>
 #include <limits.h>
 
-void popola(int array[], int *n){	
+void popola(int array[], int * n){	
 	int valore;
 	do{
 		printf("inserisci un valore. Premi -1 per terminare l'inserimento\n");	
 		scanf("%d", &valore);
-		array[*n] = valore;
-		*n++;
+		if(valore!=-1){
+			array[*n] = valore;
+			++(*n);
+		}
 	}while(valore!=-1);
 }
 
 void stampa(int array[], int n){	
+	int i;
+	for(i=0;i<n;i++)	
+		printf("Il valore è: %d\n", array[i]);
 
+}
 
+int ricerca(int array[], int n){
+	
+	int numero, posizione = -1, i;
+	printf("inserisci il numero da cercare: ");
+	scanf("%d", &numero);
+	
+	for(i=0;i<n;i++){
+		if (array[i] == numero ){
+			printf("il numero ricercato si trova in posizione: %d", i);
+			posizione = i;
+			break;
+		}
+	}
+		
+	return posizione;
 }
 	
 	
 	
 void esegui_operazione(int s, int a[], int n){
-	
+		
 	if(s==1)
 		stampa(a,n);
 	if(s==2)
-		inserisci(a,n);
+		//inserisci(a,n);
 	if(s==3)
-		modifica(a,n);	
+		//modifica(a,n);	
 	if(s==4)
-		elimina(a,n);	
+		//elimina(a,n);	
 	if(s==5)
 		ricerca(a,n);	
 			
@@ -36,34 +57,33 @@ void esegui_operazione(int s, int a[], int n){
 void menu(int array[], int n){
 
 	int scelta;
-	do{
-		printf("premere 1 per mostrare il contenuto dell'array");
-		printf("premere 2 per inserire un elemento");
-		printf("premere 3 per modificare un elemento");
-		printf("premere 4 per eliminare un elemento");
-		printf("premere 5 per ricercare un elemento");		
-		
-		scanf("%d",scelta);
-		
-		esegui_operazione(scelta,array,n);
 	
-	} while (scelta != 0)
+	printf("\npremere 1 per mostrare il contenuto dell'array\n");
+	printf("premere 2 per inserire un elemento\n");
+	printf("premere 3 per modificare un elemento\n");
+	printf("premere 4 per eliminare un elemento\n");
+	printf("premere 5 per ricercare un elemento\n");		
 	
-
-
+	scanf("%d", &scelta);
+	
+	esegui_operazione(scelta,array,n);
+	
 }
 
 
 int main(){
 	
 	int array[SHRT_MAX];
-	int n;
+	int n,scelta = 1;
 	
 	popola(array, &n);
 	
-	menu(array,n);
-		
+	while(scelta!=0){
+		menu(array,n);
+		printf("\n\npremi 0 per uscire oppure 1 per continuare ");
+		scanf("%d", &scelta);
+	}
 	
-	printf("programma terminato\n\n");
+	printf("\n\n\nprogramma terminato\n\n");
 	return 0;
 }
