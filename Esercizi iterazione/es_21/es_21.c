5f1fc95c149da6c9256d17bfb1381e8ced2b92b8
cristianogregnanin <cristiano.gregnanin@gmail.com>
2018-11-19 10:45:48 -0800

    es_21

---
+++ Esercizi iterazione/es_21/es_21.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n, i, resto; 
+	
+	do{ 
+		printf("Inserisci un numero > 1 : ");
+		scanf("%d", &n); 
+	}while(n<2); 
+	
+	i=n-1;
+	do {
+		resto = n % i;
+		if (resto == 0){
+			printf("Il numero non è primo\n");
+			return 0;
+		}
+		i = i - 1;	
+	}while(i>1);
+	
+	printf("Numero Primo\n");
+	
+	return 0;
+}
