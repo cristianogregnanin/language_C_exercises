7dc9893194a88df441e8d5c479ea56b4f44cac0a
cristianogregnanin <cristiano.gregnanin@gmail.com>
2019-01-15 13:38:14 -0800

    es 4

--- Esercizi fork/es_4/es_4.c
+++ Esercizi fork/es_4/es_4.c
@@ -10,21 +10,39 @@
 
 
 int ricerca(int numeri[], int n, int numero){
-	int posizione;
+	int posizione = -1;
 
-	//INSERT YOUR CODE HERE
+	for(posizione=0;posizione<n;posizione++){
+		if(numeri[posizione] == numero)	{
+			return posizione;
+		}
+	}
 	
-	return posizione;
+	return -1;
 }
 
 int main (int argc, char **argv){
 	
 	int numeri[] = {10,20,30};
-	int posizione;
 	int numero = atoi(argv[1]);
+	int posizione = -1;
+	
+	int pid = fork();
+	
+	if(pid == 0){
+		posizione = ricerca(numeri, 3, numero);
+		
+		if(posizione>-1)
+			printf("\n\nil numero %d si trova in posizione %d nell'array \n\n", numero, posizione);
+		else
+			printf("\n\nIl numero %d non è presente nell'array\n\n", numero);
+	
+		exit(1);
+	}else{
+		wait(&pid);
+	}
 	
 	
-	printf("\n\nMio figlio ha terminato. Il numero %d si trova in posizione %d nell'array \n\n", numero, posizione);
 	return 0;
 
 }
