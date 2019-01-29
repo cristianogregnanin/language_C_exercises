6b441a1f61bae7ebddc00982d7fb9fe848233186
cristianogregnanin <cristiano.gregnanin@gmail.com>
2019-01-29 12:48:19 -0800

    rest

--- Esercizi iterazione/es_resto/es_resto.c
+++ Esercizi iterazione/es_resto/es_resto.c
@@ -1,49 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <math.h>
 
-#define DIM 100
+int main(){
+	
+	int p=100,i;
+	int banconote[]={50,20,10,5};
+	float_t monete[]={2,1,0.5,0.2,0.1,0.05,0.02,0.01};
+	float_t n,resto;  
 
 
+    do{
+		printf ("inserisci l'importo:\n");
+		scanf ("%f",&n);
+    }while(n<=0);
+    
+    resto=p-n+0.0001;
+    printf("il resto è %.2f\n",resto);
+    
+    int monetine =  100 * (resto-(int)resto);
+    
+    printf("\n");
 
-int main (int argc, char **argv){
+    
+    for  (i=0;i<4;i++){
+		if((int)resto/(int)banconote[i]){
+			printf("le banconote da %d euro sono %d\n",banconote[i],(int)resto/(int)banconote[i]);
+			resto=(int)resto%(int)banconote[i];
+		}
+	}  
+	
+    printf("\n");
+    
+	for  (i=0;i<2;i++){
+		if(resto/monete[i]){
+			printf("le monete da %.0f euro sono %.0f\n",monete[i],(resto)/(monete[i]));
+			resto=(int)(resto)%(int)(monete[i]);
+		}		
+	}  
 
-	
-	double monete[]={20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01}, prezzo;
-	
-	sscanf(argv[1], "%lf", &prezzo);
-	
-	double resto = DIM - prezzo;
-	int i, banconote;
-	
-	sscanf(argv[1], "%lf", &resto); //REMOVE ME
-	
-	
-	for (i=0;i<5;i++){
-		banconote = resto / monete[i];
-				
-		resto = (int)resto % (int)monete[i];
-		
-		if(banconote>0)
-			printf("banconote da %2.f: %d\n", monete[i], banconote);
-		
-	}
-	
+    printf("\n");
 
-	resto = 4.7;
-	int monetine;
-	for (i=5;i<11;i++){
-		monetine = (resto*100) / (monete[i] * 100);
-				
-		resto = (int)(resto*100) % (int)(monete[i]*100);
-		
-		if(monetine>0)
-			printf("monete da %f: %d\n", monete[i], monetine);
-	
-
-		
-	}
-		
-        	
-	
-	return 0;
-
+	resto = monetine;
+	for  (i=2;i<8;i++){
+		if((int)resto/(int)(monete[i]*100)){
+			printf("le monete da %.2f euro sono %d\n",monete[i],(int)resto/(int)(monete[i]*100));
+			resto=(int)(resto)%(int)(monete[i]*100);
+		}		
+	}  
+  
+    return 0;
 }
