62ca39a28b0e7d818e1243969e30ea5ccd2b456c
cristianogregnanin <cristiano.gregnanin@gmail.com>
2019-02-05 10:39:23 -0800

    struct

---
+++ struct/es_1/soluzione_con_struct_v2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#define DIM 4
+
+
+typedef struct{
+	int eta;
+	char nome[20];
+	char cognome[20];
+} studente_t;
+
+
+studente_t older(studente_t a[], int n){
+	
+	int i,max, pos;
+	studente_t studente = a[0];
+	
+	for(i=1;i<n;i++)
+		if (a[i].eta>studente.eta)
+			studente = a[i];
+	
+	return studente;	
+}
+
+studente_t crea_studente(){
+	
+	studente_t studente;
+
+	printf("Inserisci il nome: ");	
+	scanf("%s", studente.nome);
+	
+	printf("Inserisci il cognome: ");	
+	scanf("%s", studente.cognome);
+	
+	printf("Inserisci l'eta: ");	
+	scanf("%d", &studente.eta);
+	
+	return studente;
+		
+}
+
+int main(){
+	
+	studente_t studenti[DIM];
+	int i;
+	
+	for(i=0;i<DIM;i++)
+		studenti[i] = crea_studente();
+	
+	studente_t studente = older(studenti, DIM);
+
+	printf("\nIl piu vecchio é: %s %s\n\n", studente.nome, studente.cognome);
+
+}
