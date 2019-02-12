d9ac4f600fb6acf8f12cc8da8cde52448b8095e8
cristianogregnanin <cristiano.gregnanin@gmail.com>
2019-02-12 10:51:45 -0800

    es4

---
+++ struct/es_4/es_4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#define DIM 5
+
+typedef struct{
+	char nome[20];
+	char ruolo[20];
+	char squadra[20];
+}calciatore_t;
+
+
+int main(){
+
+	calciatore_t calciatori[DIM];	
+
+	int i;
+
+	for(i=0;i<DIM;i++){
+		printf("Inserisci il nome del calciatore\n");
+		scanf("%s", calciatori[i].nome);
+		
+		printf("Inserisci il ruolo del calciatore\n");
+		scanf("%s", calciatori[i].ruolo);
+		
+		printf("Inserisci la squadra del calciatore\n");
+		scanf("%s", calciatori[i].squadra);
+			
+	}
+
+	printf("Calciatori appartenenti al milan:\n");
+	for(i=0;i<DIM;i++)
+		if(strcmp(calciatori[i].squadra, "milan")==0)
+			printf("\t%s\n", calciatori[i].nome);
+			
+	printf("Calciatori il cui nome inizia per 'c':\n");
+	for(i=0;i<DIM;i++)
+		if(calciatori[i].nome[0] == 'c')
+			printf("\t%s\n", calciatori[i].nome);
+
+}
