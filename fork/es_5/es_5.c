6d35608ec2e0eba93358b1f4ec0e9f223df2cda5
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2021-12-07 11:09:36 +0100

    es5

---
+++ fork/es_5/es_5.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+#define NOME_LENGHT 21
+#define COGNOME_LENGHT 21
+#define RUOLO_LENGHT 21
+#define MAX_LENGHT 25
+
+typedef struct
+{
+    char nome[NOME_LENGHT];
+    char cognome[COGNOME_LENGHT];
+    char ruolo[RUOLO_LENGHT];
+} Giocatore_t;
+
+int LeggiFile(char nomeFile[], int *numGiocatori, Giocatore_t giocatori[])
+{
+    FILE *stream;
+    if ((stream = fopen(nomeFile, "r")) == NULL)
+    {
+        exit(1);
+    }
+
+    for (int i = 0; !feof(stream); i++)
+    {
+        fscanf(stream, "%s %s %s\n", giocatori[*numGiocatori].nome, giocatori[*numGiocatori].cognome, giocatori[*numGiocatori].ruolo);
+        (*numGiocatori)++;
+    }
+
+    fclose(stream);
+
+    return 0;
+}
+
+void main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Argomenti errati!");
+        exit(0);
+    }
+
+    int pid, numGiocatori;
+    Giocatore_t giocatori[MAX_LENGHT];
+
+    LeggiFile(argv[1], &numGiocatori, giocatori);
+
+    pid = fork();
+    if (pid == 0)
+    {
+        for (int i = 0; i < numGiocatori; i++)
+            if (strcmp(giocatori[i].ruolo, "attaccante") == 0)
+                printf("%s %s %s\n", giocatori[i].nome, giocatori[i].cognome, giocatori[i].ruolo);
+    }
+    else
+    {
+        wait(&pid);
+        exit(0);
+    }
+}
