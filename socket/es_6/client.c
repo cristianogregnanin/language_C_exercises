e24182c332006534e00eaa760fbe62d44d7ecc6a
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2021-12-15 13:26:44 +0100

    es 6

---
+++ socket/es_6/client.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+
+void leggiFile(char nomeFile[], char testo[100][100], int *numParole)
+{
+    FILE *stream;
+
+    if ((stream = fopen(nomeFile, "r")) == NULL)
+    {
+        printf("errore nell'apertura del file input\n");
+        exit(3);
+    }
+    for (numParole; !feof(stream); (*numParole)++)
+    {
+        fscanf(stream, "%s\n", testo[*numParole]);
+    }
+    fclose(stream);
+}
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_in servizio;
+    char stringa[20];
+    char testo[100][100];
+    int cnt = 0;
+    int numParole = 0;
+    int nread, socketfd;
+
+    if (argc != 5)
+    {
+        printf("Numero di argomenti errato\n");
+        exit(2);
+    }
+
+    memset((char *)&servizio, 0, sizeof(servizio));
+
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = inet_addr(argv[1]);
+    servizio.sin_port = htons(atoi(argv[2]));
+
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    leggiFile(argv[3], testo, &numParole);
+    connect(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+
+    strcpy(stringa, argv[4]);
+    write(socketfd, testo, sizeof(testo));
+    write(socketfd, &numParole, sizeof(numParole));
+    write(socketfd, stringa, sizeof(stringa));
+    read(socketfd, &cnt, sizeof(cnt));
+    printf("La parola %s e' presente %d volte\n", stringa, cnt);
+    close(socketfd);
+}
