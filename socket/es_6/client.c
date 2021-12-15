0501dca95f12e00ffe7f42625ec789b9ae740268
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2021-12-15 20:52:09 +0100

    sistemate librerie

--- socket/es_6/client.c
+++ socket/es_6/client.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
 #include <string.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <errno.h>
 #include <unistd.h>
-#include <ctype.h>
 #include <arpa/inet.h>
+
+
 
 void leggiFile(char nomeFile[], char testo[100][100], int *numParole)
 {
--- socket/es_6/server.c
+++ socket/es_6/server.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
 #include <string.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <errno.h>
 #include <unistd.h>
-#include <ctype.h>
 
 void contaOccorrenze(char testo[100][100], char stringa[], int *cnt, int numParole)
 {
