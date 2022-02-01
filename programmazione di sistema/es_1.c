d28f065feb92ff4fc4dde57bda78a2cc3eafe815
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2022-02-01 21:39:48 +0100

    sistema

---
+++ programmazione di sistema/es_1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+    int piped[2], pid;
+    pipe(piped);
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        close(1);
+        dup(piped[1]);
+        close(piped[1]);
+        close(piped[0]);
+        execl("/bin/cat", "cat", "file.txt", (char *)0);
+    }
+
+    pid = fork();
+    if (pid == 0)
+    {
+        close(0);
+        dup(piped[0]);
+        close(piped[1]);
+        close(piped[0]);
+        execl("/bin/wc", "wc", (char *)0);
+    }
+
+    close(piped[1]);
+    close(piped[0]);
+
+    return 0;
+}
