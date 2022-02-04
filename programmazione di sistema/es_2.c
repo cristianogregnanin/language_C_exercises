34bab76d5d69b02127a4a34fc698ec02702439db
Cristiano <cristiano.gregnanin@gmail.com>
2022-02-04 10:17:38 +0100

    Create es_2.c

---
+++ programmazione di sistema/es_2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+    int piped[2], pid, status;
+    pipe(piped);
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        close(1);
+        dup(piped[1]);
+        close(piped[1]);
+        close(piped[0]);
+        execl("/bin/cat", "cat", "file.txt", (char *)0);
+    }
+
+    pid = fork();
+    if (pid == 0)
+    {
+        close(0);
+        dup(piped[0]);
+        close(piped[1]);
+        close(piped[0]);
+        execl("/bin/wc", "wc", (char *)0);
+    }
+
+    wait(&status);
+    wait(&status);
+    close(piped[1]);
+    close(piped[0]);
+
+    return 0;
+}
