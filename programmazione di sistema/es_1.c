f0005f6e2a14aba51c56b0618b4abf1cfc5a1564
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2022-02-08 19:06:58 +0100

    es1

--- programmazione di sistema/es_1.c
+++ programmazione di sistema/es_1.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-
+#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
 
-    int piped[2], pid;
+    int fd, piped[2], pid, p2p0[2];
     pipe(piped);
 
     pid = fork();
@@ -21,6 +20,7 @@ int main(int argc, char *argv[])
         execl("/bin/cat", "cat", "file.txt", (char *)0);
     }
 
+    pipe(p2p0);
     pid = fork();
     if (pid == 0)
     {
@@ -28,12 +28,28 @@ int main(int argc, char *argv[])
         dup(piped[0]);
         close(piped[1]);
         close(piped[0]);
+
+        close(1);
+        dup(p2p0[1]);
+        close(p2p0[1]);
+        close(p2p0[0]);
         execl("/bin/wc", "wc", (char *)0);
     }
 
-    wait(&pid);
     close(piped[1]);
     close(piped[0]);
+    close(p2p0[1]);
+
+    close(0);
+    dup(p2p0[0]);
+    close(p2p0[0]);
+
+    fd = open("wc.txt", O_WRONLY | O_CREAT, 0777);
+    close(1);
+    dup(fd);
+    close(fd);
+
+    execl("/usr/bin/tee","tee",(char *)0);
 
     return 0;
 }
