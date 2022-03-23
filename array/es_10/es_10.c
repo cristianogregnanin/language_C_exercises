c43e8d45f94ea80797031c1e2382fdca2d1aa80c
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2022-03-23 11:19:21 +0100

    formattazione

--- array/es_10/es_10.c
+++ array/es_10/es_10.c
@@ -5,60 +5,60 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
+    if (argc != 2)
     {
         printf("Utilizzo: Inserire una stringa di 8 caratteri \n");
         return -1;
     }
-    
-    if(strlen(argv[1]) != 8)
+
+    if (strlen(argv[1]) != 8)
     {
         printf("La lunghezza della stringa è errata, la lunghezza deve essere di 8 caratteri\n");
         return -1;
     }
-    
+
     char data[DIM];
     int i, giorno, mese, anno, k = 0;
-    
-    for(i = 0; i < 2; i++)
+
+    for (i = 0; i < 2; i++)
     {
         data[k] = argv[1][i];
         k++;
     }
     giorno = atoi(data);
-    
-    if(giorno > 31 || giorno <1)
+
+    if (giorno > 31 || giorno < 1)
     {
         printf("Data invalida \n");
         return -1;
     }
     k = 0;
-    for(i = 2; i < 4; i++)
+    for (i = 2; i < 4; i++)
     {
         data[k] = argv[1][i];
         k++;
     }
-    
+
     mese = atoi(data);
-    if(mese > 12 || mese <1)
+    if (mese > 12 || mese < 1)
     {
         printf("Data invalida \n");
         return -1;
     }
     k = 0;
-    for(i = 4; i < 8; i++)
+    for (i = 4; i < 8; i++)
     {
         data[k] = argv[1][i];
         k++;
     }
     anno = atoi(data);
-    if(anno > 2022 || anno <1)
+    if (anno > 2022 || anno < 1)
     {
         printf("Data invalida \n");
         return -1;
     }
-    
-    if(giorno < 10 && mese < 10)
+
+    if (giorno < 10 && mese < 10)
     {
         printf("0%d/0%d/%d \n", giorno, mese, anno);
     }
@@ -70,6 +70,6 @@ int main(int argc, char *argv[])
     {
         printf("%d/0%d/%d \n", giorno, mese, anno);
     }
-    
+
     return 0;
 }
