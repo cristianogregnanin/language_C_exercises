b10540bc0b90ac7a7d0bf145d83bd85d017159ad
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2022-04-01 10:02:53 +0200

    4ù

---
+++ funzioni/es_4/es_4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int ricerca_vocali(char stringa[])
+{
+
+    int vocali = 0;
+
+    for (int k = 0; k < strlen(stringa); k++)
+    {
+
+        if (stringa[k] == 'a' || stringa[k] == 'i' || stringa[k] == 'u' || stringa[k] == 'o' || stringa[k] == 'e')
+        {
+            vocali++;
+        }
+    }
+    return vocali;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("Numero argomenti sbagliato");
+        return -1;
+    }
+
+    int vocali, consonanti, totvocali = 0, totconsonanti = 0;
+
+    for (int i = 1; i < argc; i++)
+    {
+        vocali = ricerca_vocali(argv[i]);
+        consonanti = strlen(argv[i]) - vocali;
+
+        totvocali = totvocali + vocali;
+        totconsonanti = totconsonanti + consonanti;
+    }
+
+    printf("vocali: %d, consonanti: %d\n", totvocali, totconsonanti);
+
+    return 0;
+}
