a31eb813794e4bdfe90fb58f7004b880a0c0bece
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2022-10-24 08:34:42 +0200

    refactoring es 10 array

--- array/es_10/es_10.c
+++ array/es_10/es_10.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define DIM 4
+#define DIM_ANNO 4
+#define DIM_GIORNO 2
+#define DIM_MESE 2
 
 int main(int argc, char *argv[])
 {
@@ -17,15 +19,15 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    char data[DIM];
+    char str_giorno[DIM_GIORNO], str_mese[DIM_MESE], str_anno[DIM_ANNO];
     int i, giorno, mese, anno, k = 0;
 
     for (i = 0; i < 2; i++)
     {
-        data[k] = argv[1][i];
+        str_giorno[k] = argv[1][i];
         k++;
     }
-    giorno = atoi(data);
+    giorno = atoi(str_giorno);
 
     if (giorno > 31 || giorno < 1)
     {
@@ -35,11 +37,11 @@ int main(int argc, char *argv[])
     k = 0;
     for (i = 2; i < 4; i++)
     {
-        data[k] = argv[1][i];
+        str_mese[k] = argv[1][i];
         k++;
     }
 
-    mese = atoi(data);
+    mese = atoi(str_mese);
     if (mese > 12 || mese < 1)
     {
         printf("Data invalida \n");
@@ -48,10 +50,10 @@ int main(int argc, char *argv[])
     k = 0;
     for (i = 4; i < 8; i++)
     {
-        data[k] = argv[1][i];
+        str_anno[k] = argv[1][i];
         k++;
     }
-    anno = atoi(data);
+    anno = atoi(str_anno);
     if (anno > 2022 || anno < 1)
     {
         printf("Data invalida \n");
@@ -69,6 +71,10 @@ int main(int argc, char *argv[])
     else if (mese < 10)
     {
         printf("%d/0%d/%d \n", giorno, mese, anno);
+    }
+    else
+    {
+        printf("%d/%d/%d \n", giorno, mese, anno);
     }
 
     return 0;
