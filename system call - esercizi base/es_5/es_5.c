7bcb37db24ce8eee03a1ceeb379294608c8f4bc6
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2023-01-23 14:44:04 +0100

    es 4 e 5

---
+++ system call - esercizi base/es_5/es_5.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
+int calcola_lunghezza_stringa_files(int argc, char *argv[])
+{
+    int dim = 0;
+    for (int i = 1; i < argc - 2; i++)
+    {
+        dim = dim + strlen(argv[i]) + 1;
+    }
+    return dim;
+}
+
+void componi_stringa_files(int argc, char *argv[], char str[])
+{
+    str[0] = '\0';
+    for (int i = 1; i < argc - 2; i++)
+    {
+        strcat(str, argv[i]);
+        strcat(str, " ");
+    }
+    strcat(str, "\n");
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 4)
+    {
+        printf("Numero argomenti sbagliato\n");
+        exit(1);
+    }
+
+    int dim = calcola_lunghezza_stringa_files(argc, argv);
+    int fd, ct = 0, nread;
+    char carattere, output[80], stringa_files[dim];
+
+    componi_stringa_files(argc, argv, stringa_files);
+
+    for (int i = 1; i < argc - 2; i++)
+    {
+        fd = open(argv[i], O_RDONLY);
+
+        while (nread = read(fd, &carattere, sizeof(carattere)))
+        {
+            if (carattere == argv[argc - 2][0])
+            {
+                ct++;
+            }
+        }
+
+        close(fd);
+    }
+
+    sprintf(output, "Il carattere %s compare %d volte nei files: %s\n", argv[argc - 2], ct, stringa_files);
+
+    fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    write(fd, output, strlen(output));
+    close(fd);
+
+    write(1, output, strlen(output));
+
+    return 0;
+}
