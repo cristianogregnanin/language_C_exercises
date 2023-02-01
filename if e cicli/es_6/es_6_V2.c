840279d771f6bbaeeb29a8f08afd56b2f6e82349
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2023-02-01 12:27:16 +0100

    es 6 v2

---
+++ if e cicli/es_6/es_6_V2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    int i = 0, somma = 0, a, b, n;
+    double media;
+    do
+    {
+        printf("inserisci a\n");
+        scanf("%d", &a);
+        printf("inserisci b\n");
+        scanf("%d", &b);
+    } while (b < a);
+
+    while (1)
+    {
+        printf("inserisci un numero\n");
+        scanf("%d", &n);
+
+        if (n >= a && n <= b)
+        {
+            somma = somma + n;
+            i++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    media = (double)somma / (double)i;
+
+    printf("\nla media è: %f\n\n", media);
+
+    return 0;
+}
