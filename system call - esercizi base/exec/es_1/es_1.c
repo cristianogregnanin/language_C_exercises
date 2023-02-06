bbf59ad9f99c471d25e0b38fb95f5ca527530c91
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2023-02-06 08:35:04 +0100

    es 1

---
+++ system call - esercizi base/exec/es_1/es_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2)
+    {
+        printf("Numero argomenti sbagliato\n");
+        exit(1);
+    }
+
+    execl("/usr/bin/cat", "cat", argv[1], NULL);
+
+    printf("Exec ha terminato con errori");
+    return -1;
+}
