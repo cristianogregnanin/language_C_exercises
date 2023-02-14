1f366e83b9f723fb00fcf6dbb546fe240e4dc2c5
CominatoMatteo <121224252+CominatoMatteo@users.noreply.github.com>
2023-02-14 21:39:39 +0100

    Modifiche es 10 Matteo Cominato

--- 10 - if e cicli/es_10/es_10.c
+++ 10 - if e cicli/es_10/es_10.c
@@ -1,52 +1,73 @@
 #include <stdio.h>
-#include <string.h>
-
+#include <string.h> 
 int main(int argc, char *argv[])
 {
-    char tipologia[12];
-    double importo, mediaBevande, mediaRistorazioni, sommaBevande = 0, sommaRistorazione = 0;
-    int bevande = 0, ristorazione = 0;
-
+    char consumazione[13];                       
+    int bevande = 0, ristorazione = 0;          
+    int sommaBevande = 0, sommaConsumazioni = 0; 
+    float prezzoBevanda, prezzoConsumazioni;
+    float mediaBevande, mediaRistorazione;
     do
     {
-        do
+        printf("Scegli tipo consumazione \n");
+        scanf("%s", &consumazione);
+        if (strcmp(consumazione, "esci") == 0)
         {
-            printf("Inserisci la tipologia: \n");
-            scanf("%s", tipologia);
-        } while (strcmp("esci", tipologia) != 0 && strcmp(tipologia, "bevande") != 0 && strcmp(tipologia, "ristorazione") != 0);
-
-        do
-        {
-            printf("Inserisci l' importo: \n");
-            scanf("%f", &importo);
-        } while (importo <= 0);
-
-        if (strcmp(tipologia, "bevande") == 0)
-        {
-            bevande++;
-            sommaBevande = sommaBevande + importo;
+            break;
         }
-
-        if (strcmp(tipologia, "ristorazione") == 0)
+        else
         {
-            ristorazione++;
-            sommaRistorazione = sommaRistorazione + importo;
+            if (strcmp(consumazione, "bevande") == 0)
+            {
+                bevande++; 
+                do
+                {
+                    printf("Inserire prezzo della bevanda \n");
+                    scanf("%f", &prezzoBevanda); 
+                } while (prezzoBevanda <= 0);
+                sommaBevande = prezzoBevanda + sommaBevande;
+            }
+            else if (strcmp(consumazione, "ristorazione") == 0)
+            {
+                ristorazione++;
+                do
+                {
+                    printf("Inserire prezzo del piatto \n");
+                    scanf("%f", &prezzoConsumazioni); 
+                } while (prezzoConsumazioni <= 0);
+                sommaConsumazioni = sommaConsumazioni + prezzoConsumazioni; 
+            }
+            else
+            {
+                printf("scelta non valida \n");
+            }
         }
-
-    } while (strcmp("esci", tipologia) != 0);
-
-    if (ristorazione > bevande)
+    } while (1);
+    if (bevande != 0)
     {
-        printf("Sono state vendute più ristorazioni");
+        mediaBevande = (float)sommaBevande / (float)bevande;                       
+        printf("La media delle bevende vendute equivale a %2.f \n", mediaBevande); 
     }
     else
     {
-        printf("Sono state vendute più bevande");
+        printf("Non si hanno venduto bevande \n");
     }
-
-    mediaBevande = sommaBevande / (double)bevande;
-    mediaRistorazioni = sommaRistorazione / (double)ristorazione;
-
-    printf("La media delle bevande è: %f\n", mediaBevande);
-    printf("La media delle ristorazioni è: %f\n", mediaRistorazioni);
+    if (ristorazione != 0)
+    {
+        mediaRistorazione = (float)sommaConsumazioni / (float)ristorazione;
+        printf("La media dei piatti venduti equivale a %2.f \n", mediaRistorazione); 
+    }
+    else
+    {
+        printf("Non si hanno venduto piatti \n");
+    }
+    if (bevande < ristorazione)
+    {
+        printf("Hai venduto più piatti \n");
+    }
+    else
+    {
+        printf("Hai venduto più bevande \n");
+    }
+    return 0;
 }
