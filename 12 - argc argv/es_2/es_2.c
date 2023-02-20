f801ca67de9d307db9f287807c848cc6079670c0
Angelica <angelica@ubuntu2004.myguest.virtualbox.org>
2023-02-20 18:37:57 +0100

    modifica es. 2 Angelica Pigato

--- 12 - argc argv/es_2/es_2.c
+++ 12 - argc argv/es_2/es_2.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     int esponente = atoi(argv[2]);
     int risultato = 1;
 
-    if (base < 0 || esponente < 0)
+    if (base <= 0 || esponente <= 0)
     {
         printf("Inserire numeri positivi 0\n");
         exit(0);
@@ -26,4 +26,6 @@ int main(int argc, char *argv[])
     }
 
     printf("Il risultato di %d ^ %d è: %d\n", base, esponente, risultato);
+    
+    return 0;
 }
