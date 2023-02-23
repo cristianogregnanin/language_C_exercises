6ef415558d06b0864d613c69dba7d493ab144469
EmmaMilani <94352149+EmmaMilani@users.noreply.github.com>
2023-02-23 18:52:37 +0100

    es3 mio

--- programmazione di sistema/es_3/es_3.c
+++ programmazione di sistema/es_3/es_3.c
@@ -3,48 +3,36 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
-int main(int argc, char *argv[])
+int main(int argc; char*argv[])
 {
-    if (argc != 2)
-    {
-        printf("Numero parametri errato\n");
-        exit(1);
-    }
-
-    char stringa[1000], cnt[1000];
-    int p1p0[2], cnttot = 0;
-
+    int pid;
+    char word[80], tot[900];
+    int p1p0[2], only[900];
     pipe(p1p0);
-    while (1)
+    while(1)
     {
-        printf("Che parola vuoi cercare? ");
-        scanf("%s", stringa);
-
-        if (strcmp(stringa, "fine") == 0)
+        printf("Inserisci una stringa:");
+        scanf("%s", word);
+        if(strcmp(word, "fine") == 0)
         {
-            close(p1p0[1]);
             close(p1p0[0]);
-            printf("Numero di parole trovate: %d\n", cnttot);
-            exit(1);
+            close(p1p0[1]);
+            printf("Program terminated");
+            exit(0);
         }
-
-        int pid = fork();
-
-        if (pid == 0)
+        pid = fork();
+        if(pid == 0)
         {
             close(p1p0[0]);
             close(1);
             dup(p1p0[1]);
             close(p1p0[1]);
-
-            execl("/usr/bin/grep", "grep", "-c", stringa, argv[1], (char *)0);
+            execl("/usr/bin/grep", "grep", "-c", word,argv[1], NULL);
             return -1;
         }
-        read(p1p0[0], cnt, sizeof(cnt));
-        printf("Il file ha %d '%s' \n", atoi(cnt), stringa);
-        cnttot += atoi(cnt);
+        read(p1p0[0], only, sizeof(only));
+        printf("Il file ha %d di %s", atoi(only), word);
+        tot = tot + atoi(only);
     }
-
-    return 0;
+    return o;
 }
