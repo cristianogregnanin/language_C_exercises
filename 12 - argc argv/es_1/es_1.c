f3625a16a36ba5dd535a8b8aa20daf912f8d69d7
Riccardo Merlo <riccardo.merlo@iisviolamarchesini.edu.it>
2023-03-01 15:54:06 +0100

    esericizio 1 argc Merlo

--- 12 - argc argv/es_1/es_1.c
+++ 12 - argc argv/es_1/es_1.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(int argc, char *argv[])
 {
-
     if (argc != 2)
     {
         printf("Errore argomenti\n");
         exit(0);
     }
-
     int n = atoi(argv[1]);
-
     if (n <= 0)
     {
         printf("argv[1] deve essere maggiore di 0\n");
         exit(0);
     }
-
-    for (int i = 0; i < n; i++)
+    for (int k = 0; k < n; k++)
     {
-        if (i == 0 || i == n - 1)
+        for (int j = 0; j < n; j++)
         {
-            for (int k = 0; k < n; k++)
+            if (k > 0 && j > 0 && k < n - 1 && j < n - 1)
             {
-                printf("* ");
+                printf("");
+            }
+            else
+            {
+                prnitf("*");
             }
         }
-        else
-        {
-            for (int k = 0; k < n; k++)
-            {
-                if (k == 0 || k == n - 1)
-                    printf("* ");
-                else
-                    printf("  ");
-            }
-        }
-
         printf("\n");
     }
 }
