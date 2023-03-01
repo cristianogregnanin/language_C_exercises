a0fa3c877caf3c795443c3a3c5caf78bdc0b5987
Marcello <marcello@ishowspeed.myguest.virtualbox.org>
2023-03-01 14:41:42 +0100

    esercizio 1 rielaborato in classe

---
+++ 12-argc_argv/es_1/es1_rielaborato.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        printf("Errore argomenti\n");
+        exit(0);
+    }
+
+    int n = atoi(argv[1]);
+
+    if (n <= 0)
+    {
+        printf("argv[1] deve essere maggiore di 0\n");
+        exit(0);
+    }
+    for (k=0; k<n; k++)
+    {
+	for(j=0; k<n; j++)
+	{
+	    if(k>0 && j>0 && k<n-1 && j<n-1)
+	    {
+		printf(" ");
+            }else
+	    {
+		printf("*");
+	    }
+
+
+	}
+	printf("\n");
+     }
+}
