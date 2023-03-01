f338535d7be86ba11ae920407494c15b9b689d1a
Angelica <angelica@ubuntu2004.myguest.virtualbox.org>
2023-03-01 15:16:07 +0100

    es 1 argc-argv modificato Pigato

--- 12 - argc argv/es_1/es_1.c
+++ 12 - argc argv/es_1/es_1.c
@@ -18,24 +18,19 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    for (int i = 0; i < n; i++)
+    for (int k = 0; k < n; k++)
     {
-        if (i == 0 || i == n - 1)
-        {
-            for (int k = 0; k < n; k++)
-            {
-                printf("* ");
-            }
-        }
+    	for (int j = 0; j < n; j++)
+    	{
+        	if (k > 0 && j > 0 && k < n-1 && j < n-1)
+        	{
+		printf(" ");
+            	}
+
         else
         {
-            for (int k = 0; k < n; k++)
-            {
-                if (k == 0 || k == n - 1)
-                    printf("* ");
-                else
-                    printf("  ");
-            }
+               printf("*");
+        }
         }
 
         printf("\n");
