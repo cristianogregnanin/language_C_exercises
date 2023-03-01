1860ee38490566e43ccac0c072c691f69bedffff
Armando <armando@Ubuntus.myguest.virtualbox.org>
2023-03-01 14:13:34 +0100

    Modifica es 1 argc argv

--- 12 - argc argv/es_1/es_1.c
+++ 12 - argc argv/es_1/es_1.c
@@ -20,24 +20,18 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < n; i++)
     {
-        if (i == 0 || i == n - 1)
+        for (int k = 0; k < n; k++)
         {
-            for (int k = 0; k < n; k++)
+            if (k > 0 && i > 0 && k < n - 1 && i < n - 1)
             {
                 printf("* ");
             }
-        }
-        else
-        {
-            for (int k = 0; k < n; k++)
+            else
             {
-                if (k == 0 || k == n - 1)
-                    printf("* ");
-                else
-                    printf("  ");
+                printf("  ");
             }
         }
-
         printf("\n");
     }
+    return 0;
 }
