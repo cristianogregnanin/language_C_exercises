2d2ee0289c90e85f3bceb3ea4efda7a08c9eab29
DavideAggio <121225883+DavideAggio@users.noreply.github.com>
2023-03-01 14:30:51 +0100

    es 1 cambiato
    semplificato

--- 12 - argc argv/es_1/es_1.c
+++ 12 - argc argv/es_1/es_1.c
@@ -18,26 +18,20 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    for (int i = 0; i < n; i++)
+    for (int k = 0; k < n; k++)
     {
-        if (i == 0 || i == n - 1)
+        for (int j = 0; j < n; j++)
         {
-            for (int k = 0; k < n; k++)
+            if (k > 0 && j > 0 && k < n-1 && j < n-1)
             {
-                printf("* ");
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
             }
         }
-        else
-        {
-            for (int k = 0; k < n; k++)
-            {
-                if (k == 0 || k == n - 1)
-                    printf("* ");
-                else
-                    printf("  ");
-            }
-        }
-
         printf("\n");
     }
+    return 0;
 }
