f50c09f1e6786bc87e210550a6d3fc6eb0482283
David <david.negrello@iisviolamarchesini.edu.it>
2023-03-01 13:25:56 +0100

    modificheDN

--- 12 - argc argv/es_1/es_1.c
+++ 12 - argc argv/es_1/es_1.c
@@ -40,4 +40,22 @@ int main(int argc, char *argv[])
 
         printf("\n");
     }
+
+    // visto in classe
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i > 0 && j > 0 && i < n - 1 && j < n - 1)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
 }
