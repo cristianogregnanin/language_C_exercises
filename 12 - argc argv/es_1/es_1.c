a2c609058f4e838844a91d89f9a8944144878ae4
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2023-03-02 18:06:03 +0100

    Es1.c working

--- 12 - argc argv/es_1/es_1.c
+++ 12 - argc argv/es_1/es_1.c
@@ -20,17 +20,18 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int k = 0; k < n; k++)
         {
-            if (i == 0 || i == n - 1 || j == 0 || j == n - 1) 
-            {
-                printf(" *");
-            }
-            else 
+            if (i != 0 && k < n - 1 && k != 0 && i < n - 1)
             {
                 printf("  ");
+            }
+            else
+            {
+                printf(" *");
             }
         }
         printf("\n");
     }
+    return 0;
 }
