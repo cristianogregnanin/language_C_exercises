4f3362f7f4d45332fcfb5d61de46bed314a1e166
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2023-03-09 12:17:37 +0100

    es 4

--- 11 - iterazione/es_4/es_4.c
+++ 11 - iterazione/es_4/es_4.c
@@ -3,8 +3,7 @@
 int main(int argc, char *argv[])
 {
 
-	int i; // dichiarazione della variabile i
-	int n; // dichiarazione della variabile n
+	int i = 0, n;
 
 	do
 	{										   // ripeti
@@ -12,12 +11,11 @@ int main(int argc, char *argv[])
 		scanf("%d", &n);					   // leggi n
 	} while (n < 0);						   // finche n<0
 
-	i = 0; // i:=0     inizializzazione della variabile i a 0
 	do
 	{									   // ripeti
 		printf("il numero vale: %d\n", i); // scrivi i
 		i = i + 2;						   // i:=i+2
 	} while (i <= n);					   // finche i<=n
 
-	return 0; // fine
+	return 0;
 }
