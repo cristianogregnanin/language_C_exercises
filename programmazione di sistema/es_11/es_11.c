5dd1bdfc75d6bbb9f46863e4b007415cd8c02f91
andrea giuliotti <andrea.giuliotti@iisviolamarchesini.edu.it>
2023-04-11 17:15:40 +0200

    Es11 completo

--- programmazione di sistema/es_11/es_11.c
+++ programmazione di sistema/es_11/es_11.c
@@ -13,54 +13,55 @@
 
 int main(int argc, char *argv[])
 {
-    if (fork() == 0)//apertura P1
+    char ricerca[4];
+    int p1p2[2]; // pipe utilizzata per salvare l'articolo da ricercare
+    pipe(p1p2);
+
+    while (1)
     {
-        int p1p2[2]; // pipe utilizzata per salvare l'articolo da ricercare
-        pipe(p1p2);
-
-        while (1)
+        if (strcmp(ricerca, "esci") == 0) //uscita dal ciclo e conseguente chiusura del programma
         {
+            printf("Chiusura del programma.\n");
+            close(p1p2[READ]);
+            close(p1p2[WRITE]);
+            break;
+        }
 
-            char ricerca[4];
+        int pid = fork();
+        if (pid == 0) // apertura P1
+        {
+            close(p1p2[READ]);
             printf("Inserisci numero articolo da ricercare : ");
-            scanf("%s",ricerca);
-
-            if (strcmp(ricerca, "esci") == 0) // chiusura di P1
-            {
-                close(p1p2[READ]);
-                close(p1p2[WRITE]);
-                printf("Chiusura del programma.");
-                exit(0);
-            }
+            scanf("%s", ricerca);
 
             write(p1p2[WRITE], ricerca, sizeof(ricerca));
+            exit(0);
+        }
+        wait(&pid);
 
-            if (fork() == 0) // apertura P2
+        pid = fork();
+        if (pid == 0) // apertura P2
+        {
+            close(p1p2[WRITE]);
+            read(p1p2[READ], ricerca, sizeof(ricerca));
+
+            // Controllo del range degli articoli. In più, viene controllato se atoi va a buon fine o meno
+            //(la funzione atoi ritorna 0 in caso di conversione in int non riuscita).
+            if (atoi(ricerca) < 1 || atoi(ricerca) > 139)
             {
-                close(p1p2[WRITE]);
-
-                read(p1p2[READ], ricerca, sizeof(ricerca));
-
-                // Controllo del range degli articoli. In più, viene controllato se atoi va a buon fine o meno
-                //(la funzione atoi ritorna 0 in caso di conversione in int non riuscita).
-                if (atoi(ricerca) < 1 || atoi(ricerca) > 32)
-                {
-                    printf("Articolo non esistente.");
-                    exit(1);
-                }
-
-                char articolo_iniziale[LENGTH_ART], articolo_finale[LENGTH_ART];
-                char argomento_grep[LENGTH_GREP];
-
-                sprintf(articolo_iniziale, "ART. %d.", atoi(ricerca));
-                sprintf(articolo_finale, "ART. %d.", (atoi(ricerca) + 1));
-                sprintf(argomento_grep, "-P '(?<=%s)(?s).*(?=%s)", articolo_iniziale, articolo_finale);
-
-                execl("/usr/bin/grep", "grep", "-z", "-o", argomento_grep, argv[1], NULL);
-                return -1;
+                printf("Articolo non esistente.");
+                exit(1);
             }
+
+            char articolo_iniziale[LENGTH_ART], articolo_finale[LENGTH_ART], argomento_grep[LENGTH_GREP];
+            sprintf(articolo_iniziale, "ART. %d.", atoi(ricerca));
+            sprintf(articolo_finale, "ART. %d.", (atoi(ricerca) + 1));
+            sprintf(argomento_grep, "(?<=%s)(?s).*(?=%s)", articolo_iniziale, articolo_finale);
+
+            execl("/usr/bin/grep", "grep", "-z", "-o","-P", argomento_grep, argv[1], NULL);
+            return -1;
         }
     }
-
+    
     return 0;
 }
