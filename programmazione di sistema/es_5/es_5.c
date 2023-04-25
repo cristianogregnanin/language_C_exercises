877120a2c557c2722c0a3c812884f3c71faf038f
LeonardoSattin <94352754+LeonardoSattin@users.noreply.github.com>
2023-04-25 16:58:49 +0200

    Update es_5.c

--- programmazione di sistema/es_5/es_5.c
+++ programmazione di sistema/es_5/es_5.c
@@ -1,57 +1,79 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
 
-#define READ 0
-#define WRITE 1
-
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    if (argc != 2)
-    {
-        printf("Argomenti errati. Inserire come primo argomento il nome di un file\n");
-        exit(0);
-    }
-    char stringa[1000], codice[5];
-    int p1p0[2], tot = 0, pid;
+   int p1p0[2], pid, p0p1[2];
+   int y;
+   char * art_cercare[50];
+   char * art_iniz[50];
+   char * buffer[50];
+   char * art_finale[50];
+   char * argomento_grep[100];
+   char * buffer_output[50];
 
-    while (1)
-    {
-        pipe(p1p0);
-        printf("Inserisci codice:\n");
-        scanf("%s", codice);
 
-        if (strcmp("esci", codice) == 0)
-        {
-            printf("sono stati trovati: %d insoluti\n", tot);
-            close(p1p0[READ]);
-            close(p1p0[WRITE]);
-            exit(0);
-        }
+   pipe(p0p1);
+   pipe(p1p0);
 
-        pid = fork();
-        if (pid == 0)
-        {
-            close(p1p0[READ]);
-            close(WRITE);
-            dup(p1p0[WRITE]);
-            close(p1p0[WRITE]);
 
-            execl("/bin/grep", "grep", "-c", strcat(codice, " insoluto"), argv[1], NULL);
-            return -1;
-        }
+   close(p0p1[0]);
+   close(0);
+   dup(p0p1[1]);
 
-        read(p1p0[READ], stringa, sizeof(stringa));
-        printf("Sono stati trovati %d insoluti\n", atoi(stringa));
-        tot = tot + atoi(stringa);
 
-        if (pid < 0)
-        {
-            printf("Errore durante la generazione del figlio");
-        }
-    }
+   while(1)
+   {
+       printf("Inserisci l'articolo da cercare\n");
+       scanf("%s", &art_cercare);
 
-    return 0;
+
+       if(strcmp(art_cercare, "esci") == 0)
+       {
+           exit(1);
+       }
+       else
+       {
+           write(p0p1[1], art_cercare, strlength(art_cercare));
+       }
+   }
+
+
+   pid = fork();
+
+
+   if(pid == 0)
+   {
+       close(p0p1[1])/7;
+       close(1);
+       read(p0p1[0], &buffer, sizeof(buffer));
+
+
+       sprintf(art_iniz, "ART.%s.", buffer);
+       y = atoi(buffer) + 1;
+       sprintf(art_finale, "ART.%d.", y);
+       sprintf(argomento_grep, "-P '(? <= %s)(?s).*(? = %s)' ", art_iniz, art_finale);
+
+
+       close(p1p0[0]);
+       close(0);
+       dup(p1p0[1]);
+
+
+       execl("/usr/bin/grep", "-z", "-o", argomento_grep, argv[1], (char*)0);
+       return -1;
+
+
+       close(p1p0[1]);
+   }
+
+
+   close(p0p1[1]);
+   close(1);
+   read(p1p0[0], &buffer_output, sizeof(buffer_output));
+   printf("Articolo: %s\n", buffer_output);
+   return 0;
 }
