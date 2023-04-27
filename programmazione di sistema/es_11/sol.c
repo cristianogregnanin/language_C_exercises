e376b58fd147d2bc70f066a8b5252fdd5e37486d
huichao.zheng <zheng.huichao@iisviolamarchesini.edu.it>
2023-04-27 23:11:40 +0200

    es11 modificato

--- programmazione di sistema/es_11/sol.c
+++ programmazione di sistema/es_11/sol.c
@@ -11,40 +11,37 @@ int main(int argc, char* argv[])
         printf("Argomento sbagliato\n");
         return -1;
     }
-    int pid, P1P2[2];
-    char risposta[100], num[100], articolo_iniziale[100],articolo_finale[100], argomento_grep[100];
+    int pid,P1P0[2], nread;
+    char risposta[100], articolo_iniziale[100],articolo_finale[100], argomento_grep[100], buff;
 
     while(1)
     {
-        pipe(P1P2);
-        pid = fork();
-        if(pid == 0)
+        pipe(P1P0);
+        printf("Insersci il numero del articolo: \n");
+        scanf("%s", risposta);
+        if(strcmp(risposta, "Esci")==0)
         {
-            close(P1P2[0]);
-            printf("Insersci il numero del articolo: \n");
-            scanf("%s", risposta);
-            if(strcmp(risposta, "Esci")==0)
-            {
-                return 0;
-            }
-            write(P1P2[1], risposta, sizeof(risposta));
-            close(1);
+            return 0;
         }
 
         pid = fork();
         if(pid == 0)
         {
-            close(P1P2[1]);
-            read(P1P2[0],num,sizeof(num));
-            sprintf(argomento_grep, "'(?<=%s)(?s).*", num);
-            sprintf(articolo_finale, "(?=%d)'", atoi(num)+1);
+            close(P1P0[0]);         
+            close(1);
+            dup(P1P0[1]);   
+            sprintf(argomento_grep, "'(?<=%s)(?s).*", risposta);
+            sprintf(articolo_finale, "(?=%d)'", atoi(risposta)+1);
             strncat(argomento_grep, articolo_finale, sizeof(articolo_finale));
             execl("/usr/bin/grep", "grep", "-z", "-o", argomento_grep, argv[1], NULL);
             return -1;
         }
-
-        close(P1P2[0]);
-        close(P1P2[1]);
+        close(P1P0[1]);
+        while(read(P1P0[0], &buff, sizeof(buff))>0){
+            write(1,&buff,sizeof(buff));
+            printf("%c", buff);
+        }
+        close(P1P0[0]);
     }
     return 0;
 }
