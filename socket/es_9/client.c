e816c69d3ce4d773d9fc383b35eb13c524aa9905
Cesaro-Alessandro <94352342+Cesaro-Alessandro@users.noreply.github.com>
2023-10-17 19:27:26 +0200

    client

--- socket/es_9/client.c
+++ socket/es_9/client.c
@@ -3,47 +3,43 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <errno.h>
-#include <ctype.h>
 #include <unistd.h>
+#include <ctype.h>
 
-#define DIMBUFF 512
 #define SERVER_PORT 40000
 
-int main(int argc, char *argv[])
+int main(int argc, char*argv[])
 {
+    if(argc != 4)
+    {
+        printf("Numero argomenti sbagliato \n");
+        exit(1);
+    }
+    struct sockaddr_in servizio;
+    int nread, socketfd;
+    char buff[1];
+    memset((char *)&servizio, 0, sizeof(servizio));
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = inet_addr(argv[2]);
+    servizio.sin_port = htons(atoi(argv[3]));
 
-	if (argc != 4)
-	{
-		printf("Numero argomenti sbagliato\n");
-		exit(1);
-	}
-	struct sockaddr_in servizio;
-	int nread, socketfd;
-	char buff[1];
-	memset((char *)&servizio, 0, sizeof(servizio));
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    connect(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+    
+    printf("Stabilita la connessione con il server... \n");
+    write(socketfd, argv[1], strlen(argv[1]));
 
-	servizio.sin_family = AF_INET;
-	servizio.sin_port = htons(atoi(argv[3]));
-	servizio.sin_addr.s_addr = inet_addr(argv[2]);
-
-	socketfd = socket(AF_INET, SOCK_STREAM, 0);
-	connect(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
-	printf("Stabilita la connessione con il server..\n");
-
-	write(socketfd, argv[1], strlen(argv[1]));
-
-	while (nread = read(socketfd, buff, sizeof(buff)) > 0)
-	{
-		write(1, buff, nread);
-	}
-	printf("dopo il while ma prima della close");
-	close(socketfd);
-	printf("dopo  della close");
-	return 0;
+    while(nread = read(socketfd, buff, sizeof(buff))> 0)
+    {
+        write(1, buff, nread);
+    }
+    printf("\nDopo il while ma prima della close\n");
+    close(socketfd);
+    printf("Dopo della close \n");
+    return 0;
 }
