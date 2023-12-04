e371fe4f0651a9bea17d6b2f5e597d22dc08a688
RiccardoBrasiliani <94352050+RiccardoBrasiliani@users.noreply.github.com>
2023-12-05 00:09:52 +0100

    Create Server5.py

---
+++ Server5.py
@@ -0,0 +1,21 @@
+import sys
+import socket
+
+
+HOST = ""               
+PORT = int(sys.argv[1])       
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind((HOST, PORT))
+socket.listen(10)
+
+while 1:
+    conn, addr = socket.accept()
+  
+    f = conn.recv(1024).decode()
+  
+    f = [line.strip().split(',') for line in file]
+  
+    f = [{'nome': contact[0], 'cognome': contact[1], 'telefono': contact[2]} for contact in contacts]
+   
+    sorted(contacts, key=lambda x: x['nome'])
+    conn.send(f.encode())
