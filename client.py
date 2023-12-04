25595ff832c99756496e6466fd61e4d04eaf1bb7
RiccardoBrasiliani <94352050+RiccardoBrasiliani@users.noreply.github.com>
2023-12-04 23:58:35 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,27 +1,13 @@
 import socket
 import sys
-
-def start_client(server_ip, porta, stringa, carattere):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, porta))
-
-    data = f"{stringa},{carattere}"
-
-    client_socket.send(data.encode('utf-8'))
-
-    result_string = client_socket.recv(1024).decode('utf-8')
-
-    print(f"risultato della stringa è : {result_string}")
-
-    client_socket.close()
-    
-    if len(sys.argv) != 5:
-        print("rispetta questo ordine <server_ip> <porta> <stringa> <carattere>")
-        sys.exit(1)
-
-    server_ip = sys.argv[1]
-    porta = int(sys.argv[2])
-    stringa = sys.argv[3]
-    carattere = sys.argv[4]
-
-    start_client(server_ip, stringa, carattere)
+HOST = ""    
+PORT = int(sys.argv[1])             
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST, PORT))
+f=open('rubrica.txt','r')
+socket.send(f.encode)
+f2 = socket.recv(1024).decode()
+file=open('rubrica_ordinata.txt', 'w') 
+for contact in f2:
+    file.write(f"Nome: {contact['nome']}, Cognome: {contact['cognome']}, Telefono: {contact['telefono']}\n")
+socket.close()
