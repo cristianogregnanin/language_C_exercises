418f54277d11fa36555cdd0f7690341f414b845b
danielechiarionù <daniele.chiarion@iisviolamarchesini.edu.it>
2024-09-15 10:53:40 +0200

    New and changed checks in Ex 4

--- 10-if-e-cicli/es_4/es_4.c
+++ 10-if-e-cicli/es_4/es_4.c
@@ -14,19 +14,32 @@ int main(int argc, char *argv[])
 		printf("inserisci b\n");
 		scanf("%d", &b);
 
-		if (a < b)
-			r = (double)b / (double)a;
-		else
-			r = (double)a / (double)b;
+		if (a < b){
+            r = (double)b / (double)a;
+            /* prima di ogni divisione bisogna controllare che
+            il divisore sia diverso da 0,
+            altrimenti l'espressione risulterà ugualmente impossibile */
+            if(a==0){
+                printf("non si puo' dividere un numero per 0");
+                break;
+            }
+		}
+		else{
+            r = (double)a / (double)b;
+            if(b==0){
+                printf("non si puo' dividere un numero per 0");
+                break;
+            }
+		}
 
-		if (r > 0)
+		if (r >= 0)
 		{
 			r = sqrt(r);
 			printf("radice quadrata del rapporto: %f\n\n", r);
 		}
-	} while (r > 0);
-
-	printf("non è possibile calcolare la radice di un numero negativo\n\n");
+		else
+            printf("non e' possibile calcolare la radice di un numero negativo\n\n");
+	} while (r >= 0);
 
 	return 0;
 }
