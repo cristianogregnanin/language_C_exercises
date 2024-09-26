69e7e35cf4b2cfb2eb6750bbb348b8a01f834c24
danielechiarion <daniele.chiarion@iisviolamarchesini.edu.it>
2024-09-26 16:41:44 +0200

    add break and new controls

--- 10-if-e-cicli/es_10/es_10.c
+++ 10-if-e-cicli/es_10/es_10.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     double importo, mediaBevande, mediaRistorazioni, sommaBevande = 0, sommaRistorazione = 0;
     int bevande = 0, ristorazione = 0;
 
-    do
+    while (1)
     {
         do
         {
@@ -15,10 +15,15 @@ int main(int argc, char *argv[])
             scanf("%s", tipologia);
         } while (strcmp("esci", tipologia) != 0 && strcmp(tipologia, "bevande") != 0 && strcmp(tipologia, "ristorazione") != 0);
 
+        if (strcmp(tipologia, "esci") == 0)
+        {
+            break;
+        }
+
         do
         {
             printf("Inserisci l' importo: \n");
-            scanf("%f", &importo);
+            scanf("%lf", &importo);
         } while (importo <= 0);
 
         if (strcmp(tipologia, "bevande") == 0)
@@ -32,21 +37,36 @@ int main(int argc, char *argv[])
             ristorazione++;
             sommaRistorazione = sommaRistorazione + importo;
         }
-
-    } while (strcmp("esci", tipologia) != 0);
+    }
 
     if (ristorazione > bevande)
     {
-        printf("Sono state vendute più ristorazioni");
+        printf("Sono state vendute più ristorazioni\n");
     }
     else
     {
-        printf("Sono state vendute più bevande");
+        printf("Sono state vendute più bevande\n");
     }
 
-    mediaBevande = sommaBevande / (double)bevande;
-    mediaRistorazioni = sommaRistorazione / (double)ristorazione;
-
-    printf("La media delle bevande è: %f\n", mediaBevande);
-    printf("La media delle ristorazioni è: %f\n", mediaRistorazioni);
+    /* prima di calcolare la media e visualizzarla,
+    controllo che il numero di bevande e di ristorazioni sia maggiore di 0,
+    altrimenti la divisione risulterà impossibile */
+    if (bevande > 0)
+    {
+        mediaBevande = sommaBevande / (double)bevande;
+        printf("La media delle bevande è: %f\n", mediaBevande);
+    }
+    else
+    {
+        printf("Media non disponibile. Nessuna bevanda venduta\n");
+    }
+    if (ristorazione > 0)
+    {
+        mediaRistorazioni = sommaRistorazione / (double)ristorazione;
+        printf("La media delle ristorazioni è: %f\n", mediaRistorazioni);
+    }
+    else
+    {
+        printf("Media non disponibile. Nessuna ristorazione venduta\n");
+    }
 }
