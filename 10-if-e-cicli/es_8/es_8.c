af9f5e07bfef3b7745365f41805fd6481cca2440
danielechiarion <daniele.chiarion@iisviolamarchesini.edu.it>
2024-10-09 17:51:11 +0200

    ex 8 ready for doxygen

--- 10-if-e-cicli/es_8/es_8.c
+++ 10-if-e-cicli/es_8/es_8.c
@@ -1,3 +1,14 @@
+/**
+ * @brief Calcolo della somma delle radici quadrate
+ *
+ * Progettare un algoritmo che risolve il seguente problema. Si desidera
+ * calcolare la somma delle radici quadrate di N valori numerici inseriti
+ * dall’utente, con N inserito in input.
+ * L’algoritmo deve stampare la somma calcolata. L’algoritmo deve terminare
+ * con un messaggio di errore quando viene inserito un numero che non
+ * permette di effettuare il calcolo (nel dominio dei numeri reali).
+ */
+
 #include <stdio.h>
 #include <math.h>
 
