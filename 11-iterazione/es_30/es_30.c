67cc0bbd8112fffb7b3449a4ccd32157a79e8b67
Zanotti Enrico <enrico.zanotti@iisviolamarchesini.edu.it>
2024-10-21 18:04:41 +0200

    Added documentation on exercise 30

--- 11-iterazione/es_30/es_30.c
+++ 11-iterazione/es_30/es_30.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/**
+ * @brief Verifica, dati due numeri interi, che n1 sia il quadrato di n2.
+ *
+ * La funzione prende in input dall'utente i due valori, effettuando
+ * un controllo e un reinserimento in caso di valori minori o uguali
+ * a 0. Successivamente viene verificato se il primo valore e' uguale
+ * e viene stampato un risultato a seconda del risultato del controllo.
+ *
+ * @param argv Array di stringhe contenente gli argomenti da riga di comando
+ * @param argc Numero di argomenti da riga di comando
+ *
+ * @return La funzione restituisce sempre 0.
+ */
 int main(int argc, char *argv[])
 {
 
