cc07b9f113ec109634db852b7fd860d6b2451aa0
programmatoresperto <150442595+programmatoresperto@users.noreply.github.com>
2024-10-22 17:47:45 +0200

    Some changes

--- 10-if-e-cicli/es_1/es_1.c
+++ 10-if-e-cicli/es_1/es_1.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
+/** 
+* @brief Progettare un algoritmo che effettui la lettura da tastiera di una serie di coppie di valori numerici.
+*L’algoritmo deve calcolare e stampare il rapporto tra il valore minore e quello maggiore dei due.
+* Il programma termina quando uno dei due valori o entrambi sono uguali a zero.
+*/
 
 int main(int argc, char *argv[])
 {
+    int a, b;
+    double rapporto = 0;
 
-	int a, b;
-	double r;
+    do{
+        printf("Inserire il primo numero \n");
+    scanf("%d", &a);
 
-	do
-	{
-		printf("inserisci a\n");
-		scanf("%d", &a);
-		printf("inserisci b\n");
-		scanf("%d", &b);
+    printf("Inserire il secondo numero  \n");
+    scanf("%d", &b);
 
-		if (a > b)
-			r = (double)b / (double)a;
-		else
-			r = (double)a / (double)b;
-		printf("\n\tIl risultato è: %f\n\n", r);
-	} while (a != 0 && b != 0);
+    if (a > b) {
+            rapporto = (double) b / a;
+        } else {
+            rapporto = (double) a / b;
+        }
 
-	return 0;
+        printf("Il rapporto è %f \n", rapporto);
+
+    } while(a != 0 && b != 0);
+    
 }
