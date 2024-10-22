a05cb97075856123e75127ea8222ac79d65d2811
DavideLaghi <150442316+DavideFocalors79@users.noreply.github.com>
2024-10-22 17:30:53 +0200

    Add files via upload

--- 11-iterazione/es_9/es_9.c
+++ 11-iterazione/es_9/es_9.c
@@ -1,3 +1,12 @@
+/**
+ * @brief Genera e visualizza in ordine decrescente i numeri compresi da due numeri  
+ *
+ * Dati due numeri interi e positivi N1 e N2 con N2>N1, generare e
+ * visualizzare in ordine decrescente i numeri compresi tra N1 e N2.
+ *
+ * @return 0 in caso di successo.
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
@@ -9,13 +18,13 @@ int main(int argc, char *argv[])
 
 		do
 		{
-			printf("Inserisci n1 >= 1 : ");
+			printf("Inserisci il primo numero >= 1 : ");
 			scanf("%d", &n1);
 		} while (n1 < 0);
 
 		do
 		{
-			printf("Inserisci n2 >= 1 : ");
+			printf("Inserisci il secondo numero >= 1 : ");
 			scanf("%d", &n2);
 		} while (n2 < 0);
 		printf("\n\n");
