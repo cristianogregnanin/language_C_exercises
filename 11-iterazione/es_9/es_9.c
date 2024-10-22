442a43c7f30690bb73bf1ffb9b43c2f7033851b2
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2024-10-22 22:46:10 +0200

    Add Es_9 iterazione documentato

--- 11-iterazione/es_9/es_9.c
+++ 11-iterazione/es_9/es_9.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief
+ * Dati due numeri interi e positivi N1 e N2 con N2 > N1, generare e
+ * visualizzare in ordine decrescente i numeri compresi tra N1 e N2.
+ * 
+ * @return int Restituisce 0 se il programma termina correttamente.
+ * 
+ **/
 
 int main(int argc, char *argv[])
 {
@@ -6,27 +16,33 @@ int main(int argc, char *argv[])
 	int n1, n2, i;
 	do
 	{
-
 		do
 		{
 			printf("Inserisci n1 >= 1 : ");
 			scanf("%d", &n1);
+
 		} while (n1 < 0);
 
 		do
 		{
 			printf("Inserisci n2 >= 1 : ");
 			scanf("%d", &n2);
+
 		} while (n2 < 0);
 		printf("\n\n");
+
 	} while (n2 <= n1);
 
 	i = n1;
+    
 	do
 	{
 		printf("%d\n", i);
 		i = i + 1;
+
 	} while (i < n2);
+
+
 
 	return 0;
 }
