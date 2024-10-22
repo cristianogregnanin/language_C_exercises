19a1d205077da9267fe1f89db84e59db2358ae4f
filippogiatti <150442166+filippogiatti@users.noreply.github.com>
2024-10-22 22:40:15 +0200

    Update es_3.c

--- 11-iterazione/es_3/es_3.c
+++ 11-iterazione/es_3/es_3.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
+/**
+ * @brief Funzione principale del programma.
+ *
+ * Chiede all'utente di inserire un numero intero non negativo e poi stampa tutti i numeri dispari compresi tra 1 e quel numero.
+ * Se l'utente inserisce un numero negativo, il programma continua a chiedere l'input finché non viene inserito un numero positivo.
+ *
+ * @return 0 se il programma termina correttamente.
+ */
 int main(int argc, char *argv[])
 {
+    int n, i = 1;
 
-	int n, i = 1;
+    do
+    {
+        printf("Immetti un numero: ");
+        scanf("%d", &n);
+    } while (n < 0);
 
-	do
-	{
+    printf("I numeri dispari compresi fra %d e %d sono:\n\n", i, n);
+    do
+    {
+        printf("%d\n", i);
+        i = i + 2;
+    } while (i <= n);
 
-		printf("Immetti un numero: ");
-		scanf("%d", &n);
-	} while (n < 0);
-
-	printf("I numeri dispari compresi fra %d e %d sono:\n\n", i, n);
-	do
-	{
-		printf("%d\n", i);
-		i = i + 2;
-
-	} while (i <= n);
-
-	return 0;
+    return 0;
 }
