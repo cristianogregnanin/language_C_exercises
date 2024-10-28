fab118f5829e311beabf9a03c23bbfcad976b347
danielechiarion <daniele.chiarion@iisviolamarchesini.edu.it>
2024-10-28 09:11:42 +0100

    Array-Puntatori-Funzioni/array/es_4/es_4.c with documentation

--- Array-Puntatori-Funzioni/array/es_4/es_4.c
+++ Array-Puntatori-Funzioni/array/es_4/es_4.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * @brief Funzione che inverte una stringa
+ * 
+ * @param s stringa da invertire
+*/
 void inverti(char s[])
 {
 
@@ -18,6 +23,15 @@ void inverti(char s[])
 	}
 }
 
+/**
+ * @brief Si legga da riga di comando una stringa e la si mostri in output invertita.
+ * 
+ * @param argc numero di argomenti passati da riga di comando
+ * @param argv argomenti passati da riga di comando
+ * 
+ * @return -1 se il numero di argomenti passati da riga di comando è errato.
+ * 			0 in tutti gli altri casi
+*/
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
