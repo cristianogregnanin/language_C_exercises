9408d1a99312ef6f2a47805d13259fea4fb844bf
barchiii <145505905+barchiii@users.noreply.github.com>
2024-10-28 09:19:22 +0100

    Adding doxygen documentation es_7.c

--- Array-Puntatori-Funzioni/array/es_7/es_7.c
+++ Array-Puntatori-Funzioni/array/es_7/es_7.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-
+/**
+* @brief Scrivere un programma in linguaggio C che dato un array di interi mostri
+*	 in output se l'array è pari.
+*	 Esempio:
+*	 [1,1,2,1,2,1,2,2] => è pari
+*	 [1,1,2] => dispari
+*	 [3,3,4,5] => dispari
+*	 [1,2,1,2] => pari
+*
+* @param a[] Array di interi
+* @param n Numero di numeri presenti nell'array
+* 
+* @return La funzione restituisce 0 se l'array è pari
+* @return La funzione restituisce 1 se l'array è dispari
+*/
 int isPari(int a[], int n)
 {
 	int i, j, cont;
@@ -20,7 +34,20 @@ int isPari(int a[], int n)
 
 	return 0;
 }
-
+/**
+* @brief Scrivere un programma in linguaggio C che dato un array di interi mostri
+*	 in output se l'array è pari.
+*	 Esempio:
+*	 [1,1,2,1,2,1,2,2] => è pari
+*	 [1,1,2] => dispari
+*	 [3,3,4,5] => dispari
+*	 [1,2,1,2] => pari
+*
+* @param argv Array di stringhe contenente gli argomenti passati attraverso la riga di comando
+* @param argc Numero di argomenti passati attraverso la riga di comando
+* 
+* @return La funzione restituisce 0
+*/
 int main(int argc, char *argv[])
 {
 
