e2be9b6e1291235fabfa396e3b07b086302d1a28
Enrico Zanotti <enrico.zanotti@iisviolamarchesini.edu.it>
2024-10-28 09:00:39 +0100

    Added doxygen documentation for exercise 2 of 12-argc-argv

--- 12-argc-argv/es_2/es_2.c
+++ 12-argc-argv/es_2/es_2.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Si scriva un programma che calcoli l’elevazione a potenza del valore intero
+ * passato come primo argomento per il secondo argomento intero.
+ * 
+ * Esempio:
+ * 
+ * $./es 2 3
+ * 
+ * output: 2 ^ 3 = 8
+ * 
+ * @param argc Il numero di argomenti da riga di comando.
+ * Deve essere uguale a 3 per contenere nome del file, base ed esponente della potenza.
+ * @param argv Array di stringhe contenente gli argomenti da riga di comando. 
+ * @return La funzione ritorna sempre 0
+*/
 int main(int argc, char *argv[])
 {
 
