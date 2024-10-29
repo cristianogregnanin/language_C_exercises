27adfdd6269ea914cc9b3760d921c709fea10b8d
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2024-10-29 09:08:29 +0100

    documentazione

--- documentazione/html/search/searchdata.js
+++ documentazione/html/search/searchdata.js
@@ -1,24 +1,33 @@
 var indexSectionsWithContent =
 {
-  0: "hmr",
-  1: "hr",
-  2: "m",
-  3: "r"
+  0: "acdefghilmnopqrstvw",
+  1: "cgpst",
+  2: "cehrs",
+  3: "acdefilmoprsv",
+  4: "acemnpqrstv",
+  5: "cdmnprtw",
+  6: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files",
-  2: "functions",
-  3: "pages"
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "defines",
+  6: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Files",
-  2: "Functions",
-  3: "Pages"
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Macros",
+  6: "Pages"
 };
 
