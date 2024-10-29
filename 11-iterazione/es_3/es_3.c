5f5041bd3fa9ce7e2f4c28874eb9b060fc07f956
filippogiatti <150442166+filippogiatti@users.noreply.github.com>
2024-10-29 16:57:03 +0100

    Aggiunta doxygen es_3.c

--- 11-iterazione/es_3/es_3.c
+++ 11-iterazione/es_3/es_3.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/**
+ * @brief Dato N un numero intero positivo, generare e visualizzare in ordine crescente i numeri dispari
+ * minori o uguali a N.
+ *
+ * @param argv, array con all'interno i valori che vengono passati da terminale
+ * @param argc, Lunghezza dell'array argv
+ *
+ * @return 0 se il programma termina correttamente.
+*/
+
 int main(int argc, char *argv[])
 {
 
