1164d48e1815b5a39e4de188e6c665dc2b04345d
Denis <150442493+Denis0160@users.noreply.github.com>
2024-10-29 21:28:26 +0100

    es_8.c

--- 11-iterazione/es_8/es_8.c
+++ 11-iterazione/es_8/es_8.c
@@ -3,6 +3,8 @@
  * @brief Dato N un numero intero positivo, generare e visualizzare in ordine
  * decrescente i primi N numeri interi positivi.
  * 
+ * @param argc dimensione dell'array argv
+ * @param argv argomenti che prende da riga di comando
  * @return Restituisce 0 se l'esecuzione ha successo.
  */
 int main(int argc, char *argv[])
