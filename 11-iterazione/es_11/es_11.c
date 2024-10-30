3959e459ce53f5878d658e2a1543eb430cdcbd09
67lori898 <lorenzo.malachin@iisviolamarchesini.edu.it>
2024-10-30 09:17:32 +0100

    added exercise 11 doxygen documentation

--- 11-iterazione/es_11/es_11.c
+++ 11-iterazione/es_11/es_11.c
@@ -1,28 +1,31 @@
 /**
- * @brief Dato N un numero intero positivo, calcolare e visualizzare la somma dei
-    primi N numeri dispari.
+ * @brief Dato N un numero intero positivo, calcolare e visualizzare la somma dei primi N numeri dispari.
+ *
+ * @param argc Numero di argomenti da riga di comando (non utilizzato in questo programma).
+ * @param argv Array di argomenti da riga di comando (non utilizzato in questo programma).
+ *
+ * @return 0 Se il programma termina correttamente.
  */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
+    int n, i, somma;
+    do
+    {
+        printf("Inserisci un numero positivo: \n");
+        scanf("%d", &n);
+    } while (n < 0);
 
-	int n, i, somma;
-	do
-	{
-		printf("Inserisci un numero positivo: \n");
-		scanf("%d", &n);
+    somma = 0;
+    i = 1;
+    do
+    {
+        somma = somma + i;
+        i = i + 2;
+    } while (i < n);
+    printf("La somma vale: %d\n\n", somma);
 
-	} while (n < 0);
-
-	somma = 0;
-	i = 1;
-	do
-	{
-		somma = somma + i;
-		i = i + 2;
-	} while (i < n);
-	printf("La somma vale: %d\n\n", somma);
-
-	return 0;
+    return 0;
 }
