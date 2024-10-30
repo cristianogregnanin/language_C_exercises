fabb9b2b72f9660d408715ea4f71e16c9abf57a3
Mattia <mattia.pavarin@iisviolamarchesini.edu.it>
2024-10-30 20:10:22 +0100

    11-iterazione/es_17

--- 11-iterazione/es_17/es_17.c
+++ 11-iterazione/es_17/es_17.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 /**
- * @brief consegna
+ * @brief Una leggenda orientale narra di un matematico che, in cambio di alcuni servigi resi al re,
+ * chiese la seguente ricompensa: <<un chicco di riso per la prima casella di una scacchiera , 
+ * due chicchi di riso per la seconda casella di una scacchiera ,
+ * quattro chicchi di riso per la terza casella... e così via per tutte le 64 caselle della scacchiera>>.
+ * Progettare un algoritmo che, a partire dal numero n di caselle che si intendono riempire, 
+ * calcoli il numero complessivo di chicchi di riso che spettano come ricompensa.
  * @param argc è il numero di elementi inseriti dalla linea di comando
  * @param argv è un array di stringhe con gli argomenti passati da riga di comando
  * @return ritorna 0
  */
 int main(int argc, char* argv[]){
-        printf("Ciao");
+    int n;
+    unsigned long long chicchiDiRiso=1; 
+    long long chicchiCasella=1;
+
+    do{
+        printf("Quante n caselle intendi riempire?\n");
+        scanf("%d",&n);
+    }while(n>64 || n<0);
+
+    for(int i=1;i<n;i++){
+        chicchiDiRiso+=chicchiCasella;
+        chicchiCasella=chicchiCasella*2;
+   
+    }
+    printf("\nIl numero di chicchi di riso complessivo che spettano come ricompensa è : %llu\n",chicchiDiRiso);
     return 0;
 }
