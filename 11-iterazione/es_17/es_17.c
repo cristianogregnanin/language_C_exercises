fa53a1567084f910dd62f1db5e1bc33f1714b079
Mattia <mattia.pavarin@iisviolamarchesini.edu.it>
2024-10-30 15:29:08 +0100

    Caricamento esercizio 17

---
+++ 11-iterazione/es_17/es_17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * @brief consegna
+ * @param argc è il numero di elementi inseriti dalla linea di comando
+ * @param argv è un array di stringhe con gli argomenti passati da riga di comando
+ * @return ritorna 0
+ */
+int main(int argc, char* argv[]){
+        printf("Ciao");
+    return 0;
+}
