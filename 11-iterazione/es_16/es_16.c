bb79202315badb58b88c11c243bcf407731ef887
Pacu <150335631+Pacu0101@users.noreply.github.com>
2024-10-31 08:37:30 +0100

    es_16.c

--- 11-iterazione/es_16/es_16.c
+++ 11-iterazione/es_16/es_16.c
@@ -16,30 +16,33 @@
 */
 
 int main(int argv,char *argc) {
-    int n;
+  int n;
    
 
     do{
          printf("Inserisci il numero di elementi: ");
          scanf("%d", &n);
-
-        printf("Servono almeno 2 valori.\n");
+        
+        if(n<2){
+            printf("Servono almeno 2 valori.\n");
+        }
+        
     }while(n<2);
 
     
 
-    int arr[n];
+    int numeri[n];
     printf("Inserisci gli elementi in ordine crescente:\n");
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &numeri[i]);
     }
 
-    int diff = arr[1] - arr[0];  // Differenza iniziale
+    int diff = numeri[1] - numeri[0];  // Differenza iniziale
     int arimetico = 1;       // Serve per controllare se è una progressione aritmetica
     int max_diff = diff;         // Variabile per la differenza massima
 
     for (int i = 2; i < n; i++) {
-        int diff2 = arr[i] - arr[i - 1];
+        int diff2 = numeri[i] - numeri[i - 1];
         if (diff2 != diff) {
             arimetico = 0; // Non è una progressione aritmetica
         }
@@ -48,7 +51,7 @@ int main(int argv,char *argc) {
         }
     }
 
-    if (arimetico) {
+    if (arimetico==1) {
         printf("I numeri sono in progressione aritmetica con differenza: %d\n", diff);
     } else {
         printf("I numeri non sono in progressione aritmetica. Differenza massima: %d\n", max_diff);
