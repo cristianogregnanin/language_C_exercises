781061dde8d8201e3fe6e80d9811130adeba71b7
theholyemperor756 <stefano.boscolo@iisviolamarchesini.edu.it>
2024-11-03 15:29:55 +0100

    esercizio_rifatto

--- 11-iterazione/es_10/es_10.c
+++ 11-iterazione/es_10/es_10.c
@@ -1,3 +1,17 @@
+/**
+ * @brief Una birreria a fine serata deve conteggiare l’incasso. La cassiera accende il software gestionale il quale le chiede di inserire una alla volta le consumazioni servite.
+ * Le consumazioni si dividono in bevande e ristorazione. La cassiera per ogni consumazione deve inserire la tipologia di consumazione e il suo importo.
+ *
+ * Quando la cassiera inserisce la stringa “esci” il software deve mostrare a video le seguenti informazioni:
+ * 1.	La media dell’incasso delle bevande 
+ * 2.	La media dell’incasso della ristorazione
+ * 3.	Deve indicare se sono state vendute più bevande o più ristorazioni
+ * 
+ * @param argc numero di argomenti passati da riga di comando
+ * @param argv argomenti passati da riga di comando
+ * 
+ * @return la funzione restituisce 0
+ */
 #include <stdio.h>
 
 int main(int argc, char *argv[])
