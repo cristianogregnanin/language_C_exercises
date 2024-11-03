9e1d954061e084f1b36e596787261b6a249b8aaf
Mustaaa07 <150336256+Mustaaa07@users.noreply.github.com>
2024-11-03 18:09:31 +0000

    Creazione documentazione esercizio 33 iterazione

---
+++ es_33/es_33.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * @brief Progettare un algoritmo che, a partire da un numero naturale N, determini
+    se esso è perfetto o meno.
+ *
+ * @param argc Numero degli elementi passati da riga di comando.
+ * @param argv Array di stringhe degli argomenti passati da righa di comando.
+ * @return La funzione ritorna sempre 0.
+ */
+int main(int argc, char *argv[]) {
+
+    int N;
+    printf("Inserisci un numero naturale: ");
+    scanf("%d", &N);
+
+    int somma = 0;
+    for (int i = 1; i <= N / 2; i++) {
+        if (N % i == 0) {
+            somma += i;
+        }
+    }
+    if (somma==N) {
+        printf("%d è un numero perfetto.\n", N);
+    } else {
+        printf("%d non è un numero perfetto.\n", N);
+    }
+
+    return 0;
+}
