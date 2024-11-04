4e41f03332796c9bf918ec8c859343ed72953fd0
iigigii <luigi.zara@iisviolamarchesini.edu.it>
2024-11-04 18:00:08 +0100

    documentazione es 3 bis - puntatori

--- Array-Puntatori-Funzioni/puntatori/es_3/es_3_bis.c
+++ Array-Puntatori-Funzioni/puntatori/es_3/es_3_bis.c
@@ -1,3 +1,20 @@
+/**
+ * @brief In c gli array vengono passati come parametri alle funzioni 
+ * per riferimento e non per copia. Cio implica che se una funzione
+ * modifica l'array allora anche l'array originale sarà modificato.
+ * Inoltre, in linguaggio C il nome dell'array coincide con l'indirizzo
+ * del primo elemento dell'array.
+ * Quindi in C dato un array vale che:
+ * 
+ * int A[DIM];
+ * A == &A == &A[0].
+ * 
+ * @param argc Numero di argomenti da riga di comando
+ * @param argv Array di stringhe contenente gli argomenti da riga di comando
+ * 
+ * @return la funzione restituisce sempre 0
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
