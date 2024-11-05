d07e4ea31306dcea1fb399a1bcf58c24e85fa1f4
Pacu <150335631+Pacu0101@users.noreply.github.com>
2024-11-05 09:08:51 +0100

    Update es_32.c

--- 11-iterazione/es_32/es_32.c
+++ 11-iterazione/es_32/es_32.c
@@ -1,27 +1,31 @@
 /**
-* @brief Progettare un algoritmo che visualizzi tutti i divisori di un numero intero N.
-*
-* @param argc Numero di argomenti passati attraverso la riga di comando
-* @param argv argomenti passati attraverso la riga di comando
-* 
-* @return La funzione restituisce 0
-*/
+ * @brief Progettare un algoritmo che visualizzi tutti i divisori di un numero intero N.
+ *
+ * @param argc Numero di argomenti passati attraverso la riga di comando
+ * @param argv argomenti passati attraverso la riga di comando
+ *
+ * @return La funzione restituisce 0
+ */
 
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
 	int N;
 
-	do {
+	do
+	{
 		printf("Inserisci un numero intero positivo per trovarne i divisori: ");
 		scanf("%d", &N);
-	} while(N < 1);
+	} while (N < 1);
 
 	printf("\nI divisori di %d sono: ", N);
 
-	for (int i = 1; i <= N; i++) {
-		if (N % i == 0) {
+	for (int i = 1; i <= N; i++)
+	{
+		if (N % i == 0)
+		{
 			printf("%d ", i);
 		}
 	}
