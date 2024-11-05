cc9a6577be97d91d10408f978d6d0a7ce29071fd
diegorenesto <150336173+diegorenesto@users.noreply.github.com>
2024-11-05 09:09:33 +0100

    Update es_7.c

--- Array-Puntatori-Funzioni/array/es_7/es_7.c
+++ Array-Puntatori-Funzioni/array/es_7/es_7.c
@@ -16,37 +16,42 @@
 */
 int main(int argc, char *argv[])
 {
-    int pari[4];
-    pari[0] = 1;
-    pari[1] = 1;
-    pari[2] = 2;
-    pari[3] = 2;
+	int pari[4];
+	pari[0] = 1;
+	pari[1] = 1;
+	pari[2] = 2;
+	pari[3] = 2;
 
-    int i, j, cont;
-    int is_pari = 1;
+	int i, j, cont;
+	int is_pari = 1;
 
-    for (i = 0; i < 4; i++) {
-        cont = 0;
+	for (i = 0; i < 4; i++)
+	{
+		cont = 0;
 
-        
-        for (j = 0; j < 4; j++) {
-            if (pari[i] == pari[j]) {
-                cont++;
-            }
-        }
+		for (j = 0; j < 4; j++)
+		{
+			if (pari[i] == pari[j])
+			{
+				cont++;
+			}
+		}
 
-        
-        if (cont % 2 != 0) {
-            is_pari = 0;
-            break;
-        }
-    }
+		if (cont % 2 != 0)
+		{
+			is_pari = 0;
+			break;
+		}
+	}
 
-    if (is_pari) {
-        printf("L'array è pari\n");
-    } else {
-        printf("L'array è dispari\n");
-    }
+	if (is_pari)
+	{
+		printf("L'array è pari\n");
+	}
+	else
+	{
+		printf("L'array è dispari\n");
+	}
 
-    return 0;
+	return 0;
 }
