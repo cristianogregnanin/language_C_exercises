6b94c6ad99d377adce25744ee85c5d5b3e9eb2ac
Mattia <mattia.crepaldi.1e@iisviolamarchesini.edu.it>
2024-11-05 16:04:45 +0100

    Documentazione esercizio 3bis

--- Array-Puntatori-Funzioni/puntatori/es_3/es_3_bis.c
+++ Array-Puntatori-Funzioni/puntatori/es_3/es_3_bis.c
@@ -1,3 +1,12 @@
+/**
+*@brief Dato N un numero intero positivo, generare e visualizzare in ordine crescente i numeri dispari
+*		minori o uguali a N.
+*
+*@param n Un numero intero positivo (il limite superiore).
+*
+*@return 0 se l'esecuzione è completata con successo
+*/
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
