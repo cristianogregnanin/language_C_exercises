673156b8d0555b7fea6402c10b4a55fb3106c564
obbego <francesco.bego@iisviolamarchesini.edu.it>
2024-11-06 08:04:25 +0100

    correct brief on es_13/14/15/16

--- 10-if-e-cicli/es_14/es_14.c
+++ 10-if-e-cicli/es_14/es_14.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+
 /**
- * @brief Un negozio applica uno sconto del 5% per acquisti fino a 50 euro, uno sconto del 10% per acquisti tra 50 e 100 euro e uno sconto del 20% per acquisti superiori a 100 euro. Progettare un algoritmo che, a partire dall'importo totale dell'acquisto, determini lo sconto e il prezzo scontato da pagare.
+ * @brief Un negozio applica uno sconto del 5% per acquisti fino a 50 euro,
+ * uno sconto del 10% per acquisti tra 50 e 100 euro e
+ * uno sconto del 20% per acquisti superiori a 100 euro.
+ * Progettare un algoritmo che, a partire dall'importo totale dell'acquisto,
+ * determini lo sconto e il prezzo scontato da pagare.
+ *
  * @param argc Numero di argomenti della riga di comando.
  * @param argv Array di stringhe contenente gli argomenti.
+ *
  * @return ritorna zero.
  */
 
