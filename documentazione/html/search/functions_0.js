be166fb845dcebe7a0ae92fef8e855baafefbb2f
Cristiano Gregnanin <cristiano.gregnanin@gmail.com>
2024-11-07 08:14:13 +0100

    rimossa documentazione

--- documentazione/html/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['addperson_223',['addPerson',['../es__6__documentato__inglese_8c.html#aa76aad60730e7b8e9eb3f3653d2e62d8',1,'es_6_documentato_inglese.c']]],
-  ['aggiungicontatto_224',['AggiungiContatto',['../file_2es__4_2es__4_8c.html#af729e44cce59b49261cf90b15b225d82',1,'es_4.c']]]
-];
