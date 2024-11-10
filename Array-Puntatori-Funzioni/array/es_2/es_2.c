a175751186f85c3bf72c6a7c4a06a0770e8239b0
diegorenesto <150336173+diegorenesto@users.noreply.github.com>
2024-11-10 11:50:58 +0100

    Update es_2.c

--- Array-Puntatori-Funzioni/array/es_2/es_2.c
+++ Array-Puntatori-Funzioni/array/es_2/es_2.c
@@ -2,7 +2,7 @@
 #define DIM 5
 
 /**
- * @brief Chiede in input 5 numeri interi, li inserisce in un array e li stampa in ordine inverso.
+ * @brief Chiedere in input 5 numeri interi, li inserisce in un array e li stampa in ordine inverso.
  *
  * La funzione richiede all'utente di inserire 5 numeri interi, che vengono memorizzati in un array.
  * 
