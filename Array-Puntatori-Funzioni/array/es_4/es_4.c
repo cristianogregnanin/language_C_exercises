74220ada91fb479138bcfcbf03f09b6d2de78dc3
diegorenesto <150336173+diegorenesto@users.noreply.github.com>
2024-11-10 11:52:02 +0100

    Update es_4.c

--- Array-Puntatori-Funzioni/array/es_4/es_4.c
+++ Array-Puntatori-Funzioni/array/es_4/es_4.c
@@ -4,50 +4,56 @@
 /**
  * @brief Inverte una stringa di caratteri.
  * Questa funzione prende una stringa come argomento e la inverte sul posto.
- * 
+ *
  * @param s La stringa da invertire.
- * 
- * 
+ *
+ *
  */
 
 void inverti(char s[])
 {
 
-	char tmp;
-	int i, j = strlen(s) - 1;
+    char tmp;
+    int i, j = strlen(s) - 1;
 
-	for (i = 0; i < j; i++)
-	{
+    for (i = 0; i < j; i++)
+    {
 
-		tmp = s[i];
-		s[i] = s[j];
-		s[j] = tmp;
+        tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
 
-		j--;
-	}
+        j--;
+    }
 }
 
 /**
- * @brief Si legga da riga di comando una stringa e la si mostri in output invertita.
+ * @brief Si legga da riga di comando una stringa e la si mostri in output
+ * invertita. Per fare l'inversione si implementi la funzione inverti.
+
+ * Esempio:
+ * $./a.out ciao
  * 
+ * oaic
+ *
  * @param argc Numero di argomenti della riga di comando.
  * @param argv Array di stringhe che contiene gli argomenti della riga di comando.
- * 
- * @return int 0 se viene eseguito correttamente, -1 in caso di errore.
- * 
+ *
+ * @return Ritorna 0 se viene eseguito correttamente, -1 in caso di errore.
+ *
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Numero argomenti errato\n");
-		return -1;
-	}
+    if (argc != 2)
+    {
+        printf("Numero argomenti errato\n");
+        return -1;
+    }
 
-	inverti(argv[1]);
+    inverti(argv[1]);
 
-	printf("stringa invertita: %s\n", argv[1]);
+    printf("stringa invertita: %s\n", argv[1]);
 
-	return 0;
+    return 0;
 }
