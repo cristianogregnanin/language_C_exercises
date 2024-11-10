1c7c81779e6998c6b74823edcf80099bd9454fe7
xXMomyXx <150335339+xXMomyXx@users.noreply.github.com>
2024-11-10 19:35:45 +0100

    Formattato e è accentate rimpiazzate con e'

--- 11-iterazione/es_21/es_21.c
+++ 11-iterazione/es_21/es_21.c
@@ -1,6 +1,6 @@
 /**
- * @brief Dato un numero intero positivo N verificare se N è un numero primo.
- * Un numero è considerato primo solo se  è maggiore o uguale a 1, o se ha come divisori 1 e se stesso.
+ * @brief Dato un numero intero positivo N verificare se N e' un numero primo.
+ * Un numero e' considerato primo solo se e' maggiore o uguale a 1, o se ha come divisori 1 e se stesso.
  * 
  * @param argc Numero di argomenti da riga di comando
  * @param argv Array di stringhe contenente gli argomenti da riga di comando
@@ -10,30 +10,26 @@
 
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char * argv[]) {
 
-	int n, i, resto;
+  int n, i, resto;
 
-	do
-	{
-		printf("Inserisci un numero > 1 : ");
-		scanf("%d", &n);
-	} while (n < 2);
+  do {
+    printf("Inserisci un numero > 1 : ");
+    scanf("%d", & n);
+  } while (n < 2);
 
-	i = n - 1;
-	do
-	{
-		resto = n % i;
-		if (resto == 0)
-		{
-			printf("Il numero non è primo\n");
-			return 0;
-		}
-		i = i - 1;
-	} while (i > 1);
+  i = n - 1;
+  do {
+    resto = n % i;
+    if (resto == 0) {
+      printf("Il numero non è primo\n");
+      return 0;
+    }
+    i = i - 1;
+  } while (i > 1);
 
-	printf("Numero Primo\n");
+  printf("Numero Primo\n");
 
-	return 0;
+  return 0;
 }
