1e7c36b6d37469b6db5ee35903056231e9b56db0
vise <150336391+vise07@users.noreply.github.com>
2024-11-10 21:24:48 +0100

    Update es_1.c

--- Array-Puntatori-Funzioni/puntatori/es_1/es_1.c
+++ Array-Puntatori-Funzioni/puntatori/es_1/es_1.c
@@ -1,3 +1,12 @@
+/**
+ * @brief Esempio dell'utilizzo dei puntatori in linguaggio C.
+ * 
+ * @param argc Numero argomenti riga di comando
+ * @param argv Array di stringhe argomenti riga di comando
+ * 
+ * @return La funzione restituisce sempre 0.
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
@@ -14,4 +23,6 @@ int main(int argc, char *argv[])
 	printf("il contenuto di p è: %p\n", p);
 	printf("Si osservi che il contenuto di p è l'indirizzo di m\n");
 	printf("l'indirizzo di p è: %p\n\n", &p);
+
+	return 0;
 }
