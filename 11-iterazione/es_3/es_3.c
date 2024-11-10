3b7fc9cbec5572803983b158ccdbdae96ee3e5ea
theholyemperor756 <stefano.boscolo@iisviolamarchesini.edu.it>
2024-11-10 15:36:38 +0100

    correzione

--- 11-iterazione/es_3/es_3.c
+++ 11-iterazione/es_3/es_3.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/**
+ * @brief Dato N un numero intero positivo, generare e visualizzare in ordine crescente i numeri dispari
+ * minori o uguali a N.
+ * 
+ * @param argc elementi digitati da riga di comando
+ * @param argv numero di elementi digitati da riga di comando
+ * 
+ * @return la funzione restituisce 0
+ */
+
 int main(int argc, char *argv[])
 {
 
