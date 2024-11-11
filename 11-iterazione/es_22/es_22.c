2fb424afd1fa22cd13279ea8604f455c288b922a
GIANE <150336285+giane007@users.noreply.github.com>
2024-11-11 08:01:09 +0100

    Update es_22.c

--- 11-iterazione/es_22/es_22.c
+++ 11-iterazione/es_22/es_22.c
@@ -2,13 +2,13 @@
 /**
  * 
  * @brief Dato un numero N calcolare e visualizzare tutte le coppie di numeri minori di N che
- *	danno per somma il numero stesso. Non considerare la proprietà commutativa. 
+ * danno per somma il numero stesso. Non considerare la proprietà commutativa. 
  * 
  * @param argc Numero degli elementi passati da riga di comando.
  * @param argv Array di stringhe degli argomenti passati da righa di comando.
  * 
  * 
- * @return returna 0 se il programma è corretto
+ * @return ritorna 0 se il programma è corretto
  */
 
 int main(int argc, char *argv[])
