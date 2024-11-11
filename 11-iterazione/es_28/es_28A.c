03de02973154a025d2ca8093056cb28e8fdbc504
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2024-11-11 15:30:20 +0100

    Corretta documentazione es_28A.c

--- 11-iterazione/es_28/es_28A.c
+++ 11-iterazione/es_28/es_28A.c
@@ -2,11 +2,15 @@
 #include <math.h>
 
 /**
- * @brief Progettare un algoritmo per determinare lo spessore del laminato a partire dallo spessore del semilavorato grezzo e dal numero di laminatoi
- *  presenti nel processo di lavorazione.
+ * @brief In un'acciaieria il semilavorato metallico grezzo viene prodotto con uno spessore di alcuni centimetri e viene successivamente lavorato passando
+ *   per una serie di n laminatoi, ciascuno dei quali diminuisce lo spessore del 10%.
+ *  A. Progettare un algoritmo per determinare lo spessore del laminato a partire dallo spessore del semilavorato grezzo e dal numero di laminatoi
+ *   presenti nel processo di lavorazione.
+ *  B. Modificare l'algoritmo precedente in modo che determini il numero di laminatoi necessari nel processo di lavorazione per ottenere un laminato di
+ *   spessore definito a partie dallo spessore del semilavorato.
  *
  * @param argc Numero degli elementi passati da riga di comando.
- * @param argv Array di stringhe degli argomenti passati da righa di comando.
+ * @param argv Array di stringhe degli argomenti passati da riga di comando.
  * @return La funzione ritorna sempre 0.
  */
 
